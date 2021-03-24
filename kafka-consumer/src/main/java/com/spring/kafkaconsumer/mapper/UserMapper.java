dd75c72a00a6867147f645949a0bfec6953453ec
geralt1989 <raffraioli@gmail.com>
2021-03-24 11:23:41 +0100

    modify mapcostruct version in pom; modify configuration bootstrap-servers of consumer

--- kafka-consumer/src/main/java/com/spring/kafkaconsumer/mapper/UserMapper.java
+++ kafka-consumer/src/main/java/com/spring/kafkaconsumer/mapper/UserMapper.java
@@ -3,7 +3,6 @@ package com.spring.kafkaconsumer.mapper;
 import com.spring.kafkacommon.model.User;
 import com.spring.kafkaconsumer.model.UserDB;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 import org.mapstruct.Mappings;
 
 @Mapper(componentModel = "spring")
